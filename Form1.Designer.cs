cf920e177c08afeebea93a3422cf295fed328064
Michael Melikov <coutaq@gmail.com>
2020-12-24 00:54:16 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace graphics_lab_eight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDrawBar = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDrawBar)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxDrawBar, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(751, 394);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // pictureBoxDrawBar
+            // 
+            this.pictureBoxDrawBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxDrawBar.Location = new System.Drawing.Point(760, 3);
+            this.pictureBoxDrawBar.Name = "pictureBoxDrawBar";
+            this.pictureBoxDrawBar.Size = new System.Drawing.Size(37, 394);
+            this.pictureBoxDrawBar.TabIndex = 1;
+            this.pictureBoxDrawBar.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.label4, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 403);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(751, 44);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Avenir Next Cyr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(564, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 44);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Avenir Next Cyr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(190, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 44);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "10";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Avenir Next Cyr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 44);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Очки:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Avenir Next Cyr", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Стрелы:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(760, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "↺";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDrawBar)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxDrawBar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphics_lab_eight
+{
+    public partial class Form1 : Form
+    {
+        Timer bowDrawTimer;
+        Timer targetTimer;
+        int targetX = 30;
+        float bowPower = 0.0f;
+        Graphics g_bar;
+        Graphics g_canvas;
+        int mousePos;
+        public Form1()
+        {
+            InitializeComponent();
+            bowDrawTimer = new Timer();
+            bowDrawTimer.Interval = 5;
+            bowDrawTimer.Tick += new EventHandler(DrawBow);
+            targetTimer = new Timer();
+            targetTimer.Interval = 1;
+            targetTimer.Tick += new EventHandler(targetTimer_Tick);
+            targetTimer.Enabled = true;
+            g_bar = pictureBoxDrawBar.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DrawBow(object sender, EventArgs e)
+        {
+            if (bowPower <= 1)
+            {
+                Brush b = new SolidBrush(getFromHue(bowPower));
+                float height = pictureBoxDrawBar.Height-pictureBoxDrawBar.Height*bowPower;
+                g_bar.FillRectangle(b, 0, height, pictureBoxDrawBar.Width, pictureBoxDrawBar.Height/100+10);
+                bowPower+=0.02f;
+                
+            }
+        }
+        private void shootBow()
+        {
+            label2.Text = (Convert.ToInt32(label2.Text)-1).ToString();
+            int d = Math.Abs(targetX - mousePos + 30);
+            if (d <= 30)
+            {
+                MessageBox.Show("Есть попадание!");
+                label4.Text = (Convert.ToInt32(label4.Text)+((d <= 15)?10:5)).ToString();
+            }
+        }
+        int direction = 1;
+        private void targetTimer_Tick(object sender, EventArgs e)
+        {
+            int radius = 30;
+            
+            targetX += (5 * direction);
+            if (targetX >= (pictureBox1.Width - radius- 200))
+            {
+                direction = -1;
+            }
+
+            else if (targetX == radius) direction = 1;
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (label2.Text.Trim() == "0") MessageBox.Show("У вас закончились стрелы!");
+            else bowDrawTimer.Enabled = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousePos = e.X;
+            if (bowPower >= 1) shootBow();
+            bowPower = 0;
+            bowDrawTimer.Enabled = false;
+            g_bar.Clear(Color.White);
+        }
+
+        private Color getFromHue(float hue)
+        {
+            return Color.FromArgb(255, 250-(int)(250*hue), 0+(int)(200*hue), 20);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Point bowStart = new Point(pictureBox1.Width-10, pictureBox1.Height-350);
+            Point bowEnd = new Point(pictureBox1.Width - 10, pictureBox1.Height - 50);
+            g_canvas = e.Graphics;
+            g_canvas.DrawBezier(Pens.Black, bowStart.X, bowStart.Y, bowStart.X-100, (bowEnd.Y+bowStart.Y)*1/3, bowStart.X - 100, (bowEnd.Y + bowStart.Y) * 2 / 3, bowEnd.X, bowEnd.Y);
+            g_canvas.DrawLine(Pens.Black, bowStart, bowEnd);
+
+            g_canvas.FillEllipse(Brushes.Orange, new Rectangle(targetX, pictureBox1.Height / 2, 60, 60));
+            g_canvas.FillEllipse(Brushes.Red, new Rectangle(targetX+15, pictureBox1.Height/2+15, 30, 30));
+            g_canvas.DrawEllipse(Pens.Black, new Rectangle(targetX, pictureBox1.Height / 2, 60, 60));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "10";
+            label4.Text = "0";
+        }
+    }
+}
